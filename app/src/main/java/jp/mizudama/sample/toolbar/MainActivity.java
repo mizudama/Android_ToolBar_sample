f44dc293381988d0ec625a066bc3e5625002f462
Mizuho Kodama <mizuho.kodama@linecorp.com>
2015-02-12 14:28:49 +0900

    Remove unused line.

--- app/src/main/java/jp/mizudama/sample/toolbar/MainActivity.java
+++ app/src/main/java/jp/mizudama/sample/toolbar/MainActivity.java
@@ -1,9 +1,8 @@
 package jp.mizudama.sample.toolbar;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,7 +10,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -23,7 +21,6 @@ public class MainActivity extends ActionBarActivity {
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // getSupportActionBar().setDisplayHomeAsUpEnabled(true); //show back button
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
